3b012c4c71a9531ed1261c1970be29f4f8822e46
k-46 <kamalesh.a@kalvium.community>
2023-08-01 16:15:47 +0530

    fixed some bugs and added sounds

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const wicket2=document.getElementById("wickets-team2");
 
 const strikebtn=document.getElementById("strike");
 
+const strikeAudio = new Audio("http://bit.ly/so-ball-hit");
+const gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
+
 let w1=0;let w2=0;
 
 let i=0;
@@ -18,18 +21,10 @@ let s1=0;let s2=0;
 
 strikebtn.addEventListener("click",function(){
     let ran=Math.floor(Math.random()*7)+1;     
-if(i>=12){
-    if(s1>s2){
-        alert("IND Won");
-    }
-    else if(s1===s2){
-        alert("Draw Match");
-    }
-    else{
-        alert("PAK Won");
-    }
-}
-else if(i <6) {
+if(i <6) {
+    strikeAudio.pause();
+    strikeAudio.currentTime=0;
+    strikeAudio.play();
     if(ran==7){
         balls[i].textContent="W";
         w1++;
@@ -41,7 +36,10 @@ else if(i <6) {
         score1.textContent=s1;
     }
 }
-else{
+else if(i<12){
+    strikeAudio.pause();
+    strikeAudio.currentTime=0;
+    strikeAudio.play();
     if(ran==7){
         balls[i].textContent="W";
         w2++;
@@ -56,22 +54,18 @@ else{
     i++;
 if(i<7 && w1==2){
         i=6;}
-if(i>7 && w2==2){
-        i=12;}
+if(i==12 || w2==2 || s2>s1){
+    i=12;
+    gameOverAudio.play()
+    if(s1>s2){
+        alert("IND Won");
+        }
+    else if(s1===s2){
+        alert("Draw Match");
+        }
+    else{
+        alert("PAK Won");
+        }
+    
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
